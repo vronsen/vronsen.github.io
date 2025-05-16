39caf2b930db2b1d4c729eeef6c328dc82d2a323
Veronika <veronika.wehr@student.uni-luebeck.de>
2025-05-16 08:37:48 +0200

    content: first tries

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -1,9 +1,12 @@
 <script setup lang="ts">
-
+const { data } = await useAsyncData('project', () => queryCollection('content').path('/projects/${route.params.slug').first())
 </script>
 
 <template>
- 
+  <div v-if="data">
+    <h1>{{ data.title }}</h1>
+    <ContentRenderer :value="data" />
+  </div>
 </template>
 
 
